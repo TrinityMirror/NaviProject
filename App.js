f9660d1af5c78c272e86ed5ad4d7369096602721
Pongpol <trinitymirrorswitch@gmail.com>
2022-09-07 10:50:54 +0700

    792022

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, Image, StyleSheet } from 'react-native'
 
 import {
   createDrawerNavigator,
@@ -7,29 +7,33 @@ import {
   DrawerItemList,
   DrawerItem
 } from '@react-navigation/drawer';
-import { NavigationContainer,DefaultTheme } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeSceen from './Screens/HomeSceen';
+import { SafeAreaView } from 'react-native-web';
+
 
 const MyTheme = {
   ...DefaultTheme,
-  colors:{
+  colors: {
     ...DefaultTheme.colors,
-    primary:'rgb(255,45,85)'
+    primary: 'rgb(255,45,85)'
   }
 }
 
-function FeedScreen({navigation}) {
+function FeedScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Feed Screen</Text>
       <Button
         title='Open Drawer'
         onPress={() => navigation.openDrawer()}
-        />
+      />
       <Button
         title='Toggle Drawer'
         onPress={() => navigation.toggleDrawer()}
-        />
+      />
     </View>
   )
 }
@@ -37,43 +41,43 @@ function FeedScreen({navigation}) {
 function NotiScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Notification Screen</Text>
+      <Text>Notification Scren</Text>
     </View>
   )
 }
 
 function CustomDrawerContent(props) {
   return (
-    <DrawerContentScrollView{...props}>
-      <DrawerItemList{...props} />
-      <DrawerItem
-        label="Close drawer"
-        onPress={() => props.navigation.closeDrawer()}
-      />
-      <DrawerItem
-        label="Toggle drawer"
-        onPress={() => props.navigation.toggleDrawer()}
-    />
-    </DrawerContentScrollView>
+
+    <SafeAreaView style={{ flex: 1 }}>
+      <DrawerContentScrollView {...props}>
+        <Image style={styles.sideMenuProfileIcon} source={require('./assets/react-logo.png')} />
+        <DrawerItemList {...props} />
+        <DrawerItem
+          label="Close drawer"
+          onPress={() => props.navigation.closeDrawer()}
+        />
+      </DrawerContentScrollView>
+    </SafeAreaView>
   )
 }
 
 const Drawer = createDrawerNavigator();
 
-function MyDrawer(){
-  return(
-    <Drawer.Navigator 
-    useLegacyImplementation
-    drawerContent={(props)=> <CustomDrawerContent{...props}/>}
-    screenOptions={{
-      drawerStyle: {
-        backgroundColor: '#FFFF',
-        width: 240,
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent{...props} />}
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#FFFF',
+          width: 240,
         },
       }}
     >
-      <Drawer.Screen name='Feed' component={FeedScreen} />
-      <Drawer.Screen name='Article' component={NotiScreen} />
+      <Drawer.Screen name='Home' component={HomeSceen} />
+      <Drawer.Screen name='Notification' component={NotiScreen} />
     </Drawer.Navigator>
   )
 }
@@ -81,9 +85,21 @@ function MyDrawer(){
 const App = () => {
   return (
     <NavigationContainer theme={MyTheme}>
-      <MyDrawer/>
+      <MyDrawer />
     </NavigationContainer>
   );
 }
 
 export default App
+
+const styles = StyleSheet.create(
+  {
+    sideMenuProfileIcon:
+    {
+      resizeMode: 'center',
+      width: 70,
+      height: 100,
+      borderRadius: 100 / 2,
+      alignSelf: 'center',
+    },
+  })
--- Screens/HomeSceen.js
+++ Screens/HomeSceen.js
@@ -1,19 +1,42 @@
 import { View, Text, Button } from 'react-native'
-import {Ionicons} from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
 import React from 'react'
 
-const HomeSceen = ({navigation}) => {
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+  HiddenItem,
+  OverflowMenu,
+} from 'react-navigation-header-buttons';
+
+const IoniconsHeaderButton = (props) => (
+
+  <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+);
+
+const HomeSceen = ({ navigation }) => {
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item title="register" iconName="person-add" onPress={() => alert('ลงทะเบียน')} />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
+
   return (
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-    <Ionicons name='home' size={30} color='#008b8b'/>
-    <Text>Home Screen</Text>
-    <Button 
-    title='เกี่ยวกับเรา11'
-    onPress={()=>navigation.navigate('About',{
-        email:'reactnative@tni.ac.th'
-    })}
-    />
-   </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Ionicons name='home' size={30} color='#008b8b' />
+      <Text>Home Screen</Text>
+      <Button
+        title='open drawer'
+        onPress={() => navigation.openDrawer()}
+      />
+    </View>
   )
 }
 
